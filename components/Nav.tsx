4b33aaac57935ebea4352936efcace2870e588c3
Squidtoon99 <squidtoon99@gmail.com>
2022-07-08 23:31:48 -0500

    commit code while it works

---
+++ components/Nav.tsx
@@ -0,0 +1,196 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import { useRouter } from 'next/router';
+import Link from "./Link";
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import { Divider } from '@mui/material';
+
+const pages: string[] = [];
+const settings: string[] = [];
+// const pages = ['About Us', 'Join Us', 'Sponsors', 'Tournaments', 'Donate', "Contact"];
+// const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+const ResponsiveAppBar = () => {
+    const router = useRouter();
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+    const [state, setState] = React.useState(false);
+
+    const toggleDrawer =
+        (open: boolean) =>
+            (event: React.KeyboardEvent | React.MouseEvent) => {
+                if (
+                    event &&
+                    event.type === 'keydown' &&
+                    ((event as React.KeyboardEvent).key === 'Tab' ||
+                        (event as React.KeyboardEvent).key === 'Shift')
+                ) {
+                    return;
+                }
+
+                setState(open);
+            };
+
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    return (
+        <AppBar position="static" sx={{ opacity: 0.8 }}>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Box sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }}>
+                        <img src="https://cdn.discordapp.com/attachments/896446873777365012/994260156600811530/Chibi.PNG" alt="logo" width="50px" height="50px" />
+                    </Box>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component={Link}
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        Giving by Gaming
+                    </Typography>
+
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={toggleDrawer(true)}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <React.Fragment>
+                            <SwipeableDrawer
+                                anchor={"left"}
+                                open={state}
+                                onClose={toggleDrawer(false)}
+                                onOpen={toggleDrawer(true)}
+                            >
+                                <Box
+                                    sx={{ width: 250 }}
+                                    role="presentation"
+                                    onClick={toggleDrawer(false)}
+                                    onKeyDown={toggleDrawer(false)}
+                                >
+                                    <List>
+                                        <Divider />
+                                        {pages.map((text, index) => (
+                                            <ListItem key={text} onClick={() => router.push("/" + text.toLowerCase().replace(/ /g, "-"))} disablePadding>
+                                                <ListItemButton>
+                                                    <ListItemText primary={text} />
+                                                </ListItemButton>
+                                            </ListItem>
+                                        ))}
+                                    </List>
+                                </Box>
+                            </SwipeableDrawer>
+                        </React.Fragment>
+                    </Box>
+                    <Box sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }}>
+                        <img src="/Chibi.png" alt="logo" width="30px" height="30px" />
+                    </Box>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component={Link}
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'flex', md: 'none' },
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 500,
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        Giving by Gaming
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page}
+                                onClick={handleCloseNavMenu}
+                                component={Link}
+                                href={"/" + page.toLowerCase().replace(/ /g, "-")}
+                                sx={{ my: 2, color: 'white', display: 'block' }}
+                            >
+                                {page}
+                            </Button>
+                        ))}
+                    </Box>
+                    <Box sx={{ flexGrow: 0 }}>
+                        <Tooltip title="Open settings">
+                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{ mt: '45px' }}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {settings.map((setting) => (
+                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                    <Typography textAlign="center">{setting}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar >
+    );
+};
+export default ResponsiveAppBar;;
---
+++ pages/about-us.tsx
@@ -0,0 +1,25 @@
+import { Box, Container, Stack, Typography } from '@mui/material';
+import type { NextPage } from 'next';
+import Image from "next/image";
+
+const About: NextPage = () => {
+    return <>
+        <Container sx={{ p: 3 }}>
+            <Stack direction="column" p={{ xs: 6, sm: 3, md: 3 }} alignItems={{ xs: "center" }}>
+                <Typography sx={{ fontWeight: 600, flexGrow: 1 }} variant="h4" mb={3} color="textSecondary">About Us</Typography>
+                <Typography variant="h2">
+                    Our Roots
+                </Typography>
+                <Typography variant="body1" py={3}>
+                    Giving by Gaming (GG), originally known as North Texas Championship Series (NTXCS), is a nonprofit organization founded in North Texas by Amy Yan and Ethan Havemann in October of 2021. Since then, they have successfully hosted 2 tournaments, and are currently hosting their 3rd. GG has previously donated to Dallas Children's Hospital and the Muscular Dystrophy Association. As they continue to grow, they have also accepted sponsorships from AK PC Gaming Cafe (previous) and Rank 1 Gaming Cafe (current).
+                </Typography>
+                <Box maxWidth={500} flex={1}>
+                    <Image src="/OldLogo.png" alt="logo" width={500} height={500} />
+                    <p>Our original logo!</p>
+                </Box>
+            </Stack>
+        </Container>
+    </>;
+};
+
+export default About;
