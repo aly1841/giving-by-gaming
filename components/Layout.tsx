136c2cf40fd9ba3751a4fb470ca6ffc9b55e3fe2
Squidtoon99 <squidtoon99@gmail.com>
2022-07-10 12:35:28 -0500

    fixing meta content

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -10,11 +10,11 @@ const Layout = ({ children }: JSX.ElementChildrenAttribute) => {
                 <title>Giving by Gaming</title>
                 <meta name="description" content="Pay your developers more" />
                 <meta name="og:title" content="Giving by Gaming" />
-                <meta name="og:description" content="Pay your developers more" />
+                <meta name="og:description" content="Support charity by playing your favorite games in our tournament!" />
                 <meta name="og:image" content="/Chibi.png" />
                 <meta name="theme-color" content="#05a2e1" />
                 <meta name="viewport" content="initial-scale=1, width=device-width" />
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/Chibi.png" />
 
             </Head>
             <CssBaseline />
