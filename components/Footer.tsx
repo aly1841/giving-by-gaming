68ba3d4cad1dec6c236d5a04aab95560dd3a7b6f
aly1841 <109368739+aly1841@users.noreply.github.com>
2022-07-15 12:11:59 -0500

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,10 @@
 const Footer = () => {
     return <>
+        Discord: https://discord.com/invite/JGxfMh6MQ5
+        Instagram: https://instagram.comgivingbygaming/
+        Twitch: https://twitch.tv/givingbygaming
+        Twitter: https://twitter.com/givingbygaming
+        Linktree: https://linktr.ee/givingbygaming
     </>;
 };
 
