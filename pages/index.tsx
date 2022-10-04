1a2555faa98acbe971873281bcb3aeb95b78a29e
aly1841 <109368739+aly1841@users.noreply.github.com>
2022-10-03 22:24:43 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,15 +25,15 @@ const Home: NextPage = () => {
             className={styles.button}
             variant="outlined"
             color="primary"
-            href="https://docs.google.com/forms/d/e/1FAIpQLSfZgfDenmUz5KxuJJ8WI0BJo0wr_vPUKAqdiXNTUyOy_MW7sQ/viewform"
+            href="https://docs.google.com/forms/d/e/1FAIpQLSdp8TRo_2--msyzH7dPWw6bZKYBnXV25H2IF7Qfi7jAAdyoFw/viewform"
           >
-            Invidiual Registration
+            Individual Registration
           </Button>
           <Button
             className={styles.button}
             variant="outlined"
             color="primary"
-            href="https://docs.google.com/forms/d/e/1FAIpQLSdp8TRo_2--msyzH7dPWw6bZKYBnXV25H2IF7Qfi7jAAdyoFw/viewform"
+            href="https://docs.google.com/forms/d/e/1FAIpQLSfZgfDenmUz5KxuJJ8WI0BJo0wr_vPUKAqdiXNTUyOy_MW7sQ/viewform"
           >
             Team Registration
           </Button>
